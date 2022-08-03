69f9f044a19053eb978df4220a872b15cb13c08a
Brandon <93879641+beanziii@users.noreply.github.com>
2022-08-03 18:24:20 +0100

    added javascript functionality

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log(1);
+// Elements
+//
+// Inputs
+const billInput = document.getElementById('bill-input');
+const tipInput = document.getElementById('tip-input');
+const peopleInput = document.getElementById('people-input');
+//
+// Labels
+const tipLabel = document.querySelector('.tip-label');
+const totalLabel = document.querySelector('.total-label');
+//
+// Buttons
+const btnCalculate = document.querySelector('.btn-calculate');
+//
+
+// App
+//
+btnCalculate.addEventListener('click', function () {
+  const billPrice = Number(billInput.value);
+  const tipPercentage = Number(tipInput.value) / 100;
+  const numberOfPeople = Number(peopleInput.value);
+
+  if (billPrice >= 1 && tipPercentage > 0 && tipPercentage <= 100) {
+    // Calculate and show total
+    total = Math.ceil(calculateTotal(billPrice, tipPercentage));
+    console.log(total);
+    totalLabel.textContent = `£${total}`;
+    totalLabel.style.opacity = 100;
+
+    // Calculate and show per person
+    const perPerson = Math.ceil(calculatePerPerson(total, numberOfPeople));
+    tipLabel.textContent = `£${perPerson}`;
+    tipLabel.style.opacity = 100;
+  }
+});
+
+const calculateTotal = (bill, tip) => bill * tip + bill;
+const calculatePerPerson = (total, people) => total / people;
