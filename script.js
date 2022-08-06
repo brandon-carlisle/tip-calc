e1516df894239bc483176c96a4836715a108f003
Brandon <93879641+beanziii@users.noreply.github.com>
2022-08-06 16:32:27 +0100

    remove comments

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-// Elements
-//
-// Inputs
 const billInput = document.getElementById('bill-input');
 const tipInput = document.getElementById('tip-input');
 const peopleInput = document.getElementById('people-input');
-//
-// Labels
+
 const tipLabel = document.querySelector('.tip-label');
 const totalLabel = document.querySelector('.total-label');
-//
-// Buttons
-const btnCalculate = document.querySelector('.btn-calculate');
-//
 
-// App
-//
+const btnCalculate = document.querySelector('.btn-calculate');
+
 btnCalculate.addEventListener('click', function () {
   const billPrice = Number(billInput.value);
   const tipPercentage = Number(tipInput.value) / 100;
   const numberOfPeople = Number(peopleInput.value);
 
   if (billPrice >= 1 && tipPercentage > 0 && tipPercentage <= 100) {
-    // Calculate and show total
     total = calculateTotal(billPrice, tipPercentage).toFixed(2);
     console.log(total);
     totalLabel.textContent = `£${total}`;
     totalLabel.style.opacity = 100;
 
-    // Calculate and show per person
     const perPerson = calculatePerPerson(total, numberOfPeople).toFixed(2);
     tipLabel.textContent = `£${perPerson}`;
     tipLabel.style.opacity = 100;
